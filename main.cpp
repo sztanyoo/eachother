5b2ee7ae08c2356b48f8e703daf569b320feb330
Sztankovszky Bence <sztanyoo@gmail.com>
2013-06-21 14:40:31 +0200

    two classes holding pointers to each other

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class B;
+
+class A
+{
+  public:
+    void setB( B* b );
+    A();
+    void speak();
+    B* getB();
+  private:
+    shared_ptr<B*> m_b;
+};
+
+A::A() : m_b( NULL )
+{}
+
+void A::setB( B* b )
+{
+  m_b = b;
+}
+
+void A::speak()
+{
+  cout << "A" << endl;
+}
+
+B* A::getB()
+{
+  return m_b;
+}
+
+class B
+{
+  public:
+    void setA( A* a);
+    A* getA();
+    void speak();
+    B();
+  private:
+    A* m_a;
+};
+
+B::B() : m_a( NULL )
+{}
+
+A* B::getA()
+{
+  return m_a;
+}
+
+void B::speak()
+{
+  cout << "B" << endl;
+}
+
+void B::setA( A* a )
+{
+  m_a = a;
+}
+
+int main()
+{
+  A* a = new A();
+  B* b = new B();
+  a->setB( b );
+  b->setA( a );
+  cout << "a: " << a << " B.a: " << b->getA() << endl;
+  cout << "b: " << b << " A.b: " << a->getB() << endl;
+  a->getB()->getA()->speak();
+  b->getA()->getB()->speak();
+
+  delete a;
+  delete b;
+
+  return 0;
+}
+
+
